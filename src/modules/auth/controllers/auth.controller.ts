70645e47c7df3f1342a4e33cc19c34c915aa3f8d
Arthur Gabriel <arthurgabriel73@gmail.com>
2022-11-20 22:21:03 -0300

    feat: implements validate token rout

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -3,11 +3,15 @@ import {
     Controller,
     Post,
     UseGuards,
-    Body } from '@nestjs/common';
+    Body, Param, Get
+} from '@nestjs/common';
 import { LoginDto } from '../dtos/login.dto';
 import {AuthService} from "../services/auth.service";
 import {LocalAuthGuard} from "../security/local-auth.guard";
 import {ApiTags} from "@nestjs/swagger";
+import {CashOutDto} from "../../transactions/dtos/cash-out.dto";
+import {Transaction} from "../../transactions/entities/transactions.entity";
+import {JwtAuthGuard} from "../security/jwt-auth.guard";
 
 export const AUTH_URL = "/auth"
 
@@ -22,5 +26,11 @@ export class AuthController {
     @Post("login")
     async login(@Request() req, @Body() body: LoginDto) {
         return this.authService.login(req.user)
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('validate')
+    async validateToken(): Promise<boolean> {
+        return true
     }
 }
